3e9248552f994a39270734dffddb5c483978a829
admin <sketchover@hotmail.com>
2016-07-11 09:02:32 +0700

    gggg

--- app/src/main/java/com/zeiyu/simplememo/activity/MainActivity.java
+++ app/src/main/java/com/zeiyu/simplememo/activity/MainActivity.java
@@ -60,7 +60,9 @@ public class MainActivity extends AppCompatActivity {
         // toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        */
 
+        /*
         // floataction
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
